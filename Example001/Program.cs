54c09be429b618cde132a13d33645856d6e1783d
Oksana <79263143446@mail.ru>
2021-10-29 12:12:21 +0300

    example1

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Какая задача.
+// 2. Я делаю то и то...
+// 3. Какие данные
+// 4. Что вы ожидаете
+// 5. Что получается в итоги
+// 6. Ошибки
+// 7. Почему как исправить?
+
+// int, double, string, bool
+
+// Переменная
+// if else
+// while
+
+// поменять значение двух переменных местами
+
+int a=1;
+int b=3;
+
+Console.WriteLine("a = " + a + " b = " + b);
+
+int temp=a;
+a=b;
+b=temp;
+
+Console.WriteLine("a = {0}  b = {1}", a, b);
+Console.WriteLine($"a = {a}  b = {b}");
+
